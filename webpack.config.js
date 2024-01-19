c9ec1c6dd66e14af5343087d8cdd0c74e2503ef1
Nikhil Vishwakarma <63867254+vishwakarmanikhil@users.noreply.github.com>
2024-01-19 20:41:57 +0530

    working on frontend deployment

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     },
     devServer: {
         port: 3000,
-        historyApiFallback: true
+        historyApiFallback: true,
+        disableHostCheck: true,
     },
     module: {
         rules: [
